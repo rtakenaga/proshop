1fd06037e694095d1e668ea9fbbc08970151e7fc
Ryoma Takenaga <kingriobird@Ryomas-MacBook-Pro.local>
2021-12-08 21:07:14 -0500

    session 15

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const connecftDB = async () => {
+
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true, 
+            useNewUrlParse: true, 
+            useCreateIndex: true
+        })
+
+        console.log('MongoDB Connected: ${conn.connection.host}')
+    } catch (error) {
+        console.error('Error: ${error.message}')
+        process.exit(1)
+    }
+
+}
+
+export default connectDB
