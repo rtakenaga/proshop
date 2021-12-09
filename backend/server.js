eb62f4751ee9247d0312f5fdf5c51e2e8e0a3453
Ryoma Takenaga <kingriobird@Ryomas-MacBook-Pro.local>
2021-12-08 21:15:43 -0500

    session16

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -23,4 +24,4 @@ app.get('/api/products/:id',(req, res) => {
 
 const  PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log('Server running in port ${process.env.NODE._ENV} mode on port ${PORT}'))
+app.listen(PORT, console.log('Server running in port ${process.env.NODE._ENV} mode on port ${PORT}'.yellow.bold))
